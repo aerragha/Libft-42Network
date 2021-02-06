eb6b77a5bcca3b152c86a4fc0a2af897f06318a8
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:27 +0100

    add ft_strnew.c

---
+++ ft_strnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/07 10:51:46 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/15 19:23:48 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	size_t	i;
+	char	*str;
+
+	if (!(str = ft_memalloc(size + 1)))
+		return (NULL);
+	i = 0;
+	while (i < size)
+		str[i++] = '\0';
+	return (str);
+}
