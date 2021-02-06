0ae9412086063480ac705d5c4f903be48f7436bc
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:27 +0100

    add ft_strncpy.c

---
+++ ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/05 14:51:41 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/05 16:05:59 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncpy(char *dest, const char *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < len)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
