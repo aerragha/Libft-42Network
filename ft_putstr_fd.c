ff6b29ddceaf40d1f7d4f134d87ba860cef3d3be
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:26 +0100

    add ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/06 18:39:19 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/09 08:53:48 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putstr_fd(char const *str, int fd)
+{
+	if (str)
+		write(fd, str, sizeof(char) * ft_strlen(str));
+}
