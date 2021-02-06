097dd112f6b0fe047cd65c8f37d65283987774aa
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:26 +0100

    add ft_lstdel.c

---
+++ ft_lstdel.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/09 11:15:41 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/16 09:35:45 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *ptr;
+	t_list *tmp;
+
+	ptr = *alst;
+	while (ptr)
+	{
+		tmp = ptr;
+		ft_lstdelone(&ptr, del);
+		ptr = tmp->next;
+	}
+	*alst = NULL;
+}
