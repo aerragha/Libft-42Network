01b855290afda00907ab9509fb9fb1a374d68a0f
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:27 +0100

    add ft_strequ.c

---
+++ ft_strequ.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/07 09:28:44 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/11 13:03:02 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strequ(char const *s1, char const *s2)
+{
+	unsigned int i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (s1 == s2 ? 1 : 0);
+	if (ft_strlen((char*)s1) != ft_strlen((char*)s2))
+		return (0);
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
