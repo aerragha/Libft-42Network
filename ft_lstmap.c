3d9786ab7907020654fa7f3492a84c3735710d68
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:26 +0100

    add ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/13 20:11:29 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/13 20:14:26 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new;
+
+	if (lst)
+	{
+		new = f(lst);
+		new->next = ft_lstmap(lst->next, f);
+		return (new);
+	}
+	return (NULL);
+}
