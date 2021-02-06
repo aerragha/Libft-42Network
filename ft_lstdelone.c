b4218b8405d7ae10973512bc37bb6c6c550bb00f
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:26 +0100

    add ft_lstdelone.c

---
+++ ft_lstdelone.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/09 11:05:41 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/10 08:12:33 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+}
