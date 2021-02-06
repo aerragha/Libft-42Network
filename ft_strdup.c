1a94632c3a5524adb55063ff401afc9f49c68424
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:27 +0100

    add ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/05 14:28:34 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/08 17:12:42 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*new;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (s1[size])
+		size++;
+	if (!(new = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
