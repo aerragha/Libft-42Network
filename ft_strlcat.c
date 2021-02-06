98f3406061b157504e8cf4d1614ffb594652399d
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 11:45:27 +0100

    add ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerragha <zeehindi@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/05 15:26:56 by aerragha          #+#    #+#             */
+/*   Updated: 2018/10/06 16:42:13 by aerragha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	nb;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	nb = 0;
+	while (src[nb] != '\0')
+		nb++;
+	if (size <= i)
+		nb = nb + size;
+	else
+		nb = nb + i;
+	j = 0;
+	while (src[j] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (nb);
+}
